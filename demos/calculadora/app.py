a0bf3ba291f59b0d25442747ce6e09de9c5d77fe
Mayra994 <1725110276@utectulancingo.edu.mx>
2026-06-10 14:51:02 +0000

    CREATED configuracion del virtual environment

--- demos/calculadora/app.py
+++ demos/calculadora/app.py
@@ -14,12 +14,30 @@ class Index:
 
 class Calculadora:
     def GET(self):
-        return render.calculadora()
+        numero_1=0.0
+        numero_2=0.0
+        resultado=0.0
+        return render.calculadora(numero_1, numero_2, resultado)
+    
     def POST(self):
         formulario = web.input()
-        return f"Formulario:{formulario}"
+        numero_1 = float(formulario ['numero_1'])
+        numero_2 = float(formulario ['numero_2'])
+        resultado = numero_1 + numero_2
+
+    def POST(self):
+        formulario = web.input()
+        numero_1 = float(formulario ['numero_1'])
+        numero_2 = float(formulario ['numero_2'])
+        resultado = numero_1 - numero_2
 
     
 
+        print(f"Tipo de dato de numero_1: {type(numero_1)}")
+        print(f"Tipo de dato de numero_2: {type(numero_2)}")
+
+        return render.calculadora(numero_1, numero_2, resultado)
+  
+    
 if __name__ == "__main__":
     app.run()
